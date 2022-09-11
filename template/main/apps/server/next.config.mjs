00733852708187d41e02e68a393253b687943cc2
OrJDev <orjdeveloper@gmail.com>
2022-09-11 21:58:59 +0300

    modified nextjs cors

--- template/main/apps/server/next.config.mjs
+++ template/main/apps/server/next.config.mjs
@@ -17,5 +17,24 @@ export default withTM(["api", "db"])(
   defineNextConfig({
     reactStrictMode: false,
     swcMinify: true,
+    async headers() {
+      return [
+        {
+          source: "/api/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            {
+              key: "Access-Control-Allow-Methods",
+              value: "GET, POST, OPTIONS",
+            },
+            {
+              key: "Access-Control-Allow-Headers",
+              value: "Content-Type",
+            },
+          ],
+        },
+      ];
+    },
   })
 );
--- template/main/apps/server/src/pages/api/trpc/[trpc].ts
+++ template/main/apps/server/src/pages/api/trpc/[trpc].ts
@@ -1,10 +1,23 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { appRouter as router, createContext } from "api";
-import { withCors } from "~/utils/cors";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default withCors(
-  createNextApiHandler({
-    router,
-    createContext,
-  })
-);
+const trpcHandler = createNextApiHandler({
+  router,
+  createContext,
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.statusCode = 200;
+    return res.end();
+  }
+  return await trpcHandler(req, res);
+}
