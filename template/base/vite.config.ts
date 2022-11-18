cd72d1c7e4d6947e894f5e71f6f15c9dad424463
OrJDev <orjdeveloper@gmail.com>
2022-11-18 19:23:56 +0200

    fix github wtf lines

--- template/base/vite.config.ts
+++ template/base/vite.config.ts
@@ -1,15 +1,10 @@
 import solid from "solid-start/vite";
-import dotenv from "dotenv";
 import { defineConfig } from "vite";
 import dotenv from "dotenv";
 
 export default defineConfig(() => {
   dotenv.config();
   return {
-<<<<<<< HEAD
     plugins: [solid({ ssr: true })],
-=======
-    plugins: [solid({ ssr: false })],
->>>>>>> 88a4a00418f8cf9b23c66d8e4fdb215b8d0007fc
   };
 });
