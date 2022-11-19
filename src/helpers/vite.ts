c1923fa6f76e4c598948013fafa9fd7f3ddcdab6
CDE90 <cdeyolo@outlook.com>
2022-11-19 18:50:53 +0000

    Make more readable & add vercel only option

--- src/helpers/vite.ts
+++ src/helpers/vite.ts
@@ -5,15 +5,20 @@ import { ICtx, IUtil } from "~types";
 export const getViteConfig: IUtil = (ctx) => {
   const useUno = ctx.installers.includes("UnoCSS");
   const shouldUseSSR = !ctx.installers.includes("tRPC");
-  const plugins =
-    useUno && ctx.vercel
-      ? `[
-      solid({ ssr: ${shouldUseSSR}, adapter: vercel({ edge: false }) }),
-      UnoCSS(),
-    ]`
-      : useUno
-      ? `[solid({ ssr: ${shouldUseSSR} }), UnoCSS()]`
-      : `[solid({ ssr: ${shouldUseSSR} })]`;
+  const plugins = (() => {
+    if (useUno && ctx.vercel) {
+      return `[
+        solid({ ssr: ${shouldUseSSR}, adapter: vercel({ edge: false }) }),
+        UnoCSS(),
+      ]`;
+    } else if (useUno) {
+      return `[solid({ ssr: ${shouldUseSSR} }), UnoCSS()]`;
+    } else if (ctx.vercel) {
+      return `[solid({ ssr: ${shouldUseSSR}, adapter: vercel({ edge: false }) })]`;
+    } else {
+      return `[solid({ ssr: ${shouldUseSSR} })]`;
+    }
+  })();
   return `import solid from "solid-start/vite";
 import dotenv from "dotenv";${
     useUno ? `\nimport UnoCSS from "unocss/vite";` : ""
