bfa83fcb01dd7222d6be3e72242d331e822fc305
aidanaden <aidanaden@hotmail.com>
2022-11-24 15:55:26 +0800

    feat: re-add upstash ratelimit

---
+++ src/installers/Upstash Ratelimit/index.ts
@@ -0,0 +1,35 @@
+import { IInstaller } from "~types";
+
+const config: IInstaller = (ctx) => ({
+  name: "Upstash Ratelimit",
+  env: [
+    {
+      key: "UPSTASH_REDIS_REST_URL",
+      type: "string()",
+      kind: "server",
+    },
+    {
+      key: "UPSTASH_REDIS_REST_TOKEN",
+      type: "string()",
+      kind: "server",
+    },
+  ],
+  files: [
+    ctx.installers.includes("tRPC")
+      ? {
+          path: `${__dirname}/files/trpcUtils.txt`,
+          to: `${ctx.userDir}/src/server/trpc/utils.ts`,
+        }
+      : {
+          path: `${__dirname}/files/api.txt`,
+          to: `${ctx.userDir}/src/routes/api/limit.ts`,
+        },
+  ],
+  pkgs: {
+    "@upstash/ratelimit": {},
+    "@upstash/redis": {},
+  },
+  after: "tRPC",
+});
+
+export default config;
