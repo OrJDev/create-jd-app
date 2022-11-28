804ee940a35904b266efe7e6f8f8730e81c9f188
aidanaden <aidanaden@hotmail.com>
2022-11-28 13:47:16 +0800

    feat: add fetchRequestHandler to trpc api route

--- template/trpc/api/trpc/[trpc].ts
+++ template/trpc/api/trpc/[trpc].ts
@@ -1,11 +1,17 @@
-import { createSolidAPIHandler } from "solid-start-trpc";
-import { createContext } from "~/server/trpc/context";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import type { APIEvent } from "solid-start";
 import { appRouter } from "~/server/trpc/router/_app";
 
-const handler = createSolidAPIHandler({
-  router: appRouter,
-  createContext,
-});
+const handler = (ctx: APIEvent) =>
+  fetchRequestHandler({
+    router: appRouter,
+    createContext: () => ({
+      req: ctx.request,
+      res: { headers: {} },
+    }),
+    endpoint: "/api/trpc",
+    req: ctx.request,
+  });
 
 export const GET = handler;
 export const POST = handler;
